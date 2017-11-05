bbce15c261450fd5a7f8e3ef1d8e1f16d68e5908
Julio Sene França <julio.sene@gmail.com>
2017-11-05 21:50:54 -0200

    Update install-nginx-php.sh

--- install-nginx-php.sh
+++ install-nginx-php.sh
@@ -4,6 +4,10 @@ apt-get -fy dist-upgrade
 apt-get -fy upgrade
 apt-get install -y lsb-release bc
 
+SharedStorageAccountName=$1
+SharedAzureFileName=$2
+SharedStorageAccountKey=$3
+
 REL=`lsb_release -sc`
 DISTRO=`lsb_release -is | tr [:upper:] [:lower:]`
 NCORES=` cat /proc/cpuinfo | grep cores | wc -l`
@@ -25,6 +29,16 @@ apt-get install -fy php-gd
 # replace www-data to nginx into /etc/php/7.0/fpm/pool.d/www.conf
 # sed -i 's/www-data/nginx/g' /etc/php/7.0/fpm/pool.d/www.conf
 service php7.0-fpm restart
+
+mkdir /var/www/html/autoscale
+
+if [[ -z "$SharedStorageAccountName" ]] || [[ -z "$SharedAzureFileName" ]] || [[ -z "$SharedStorageAccountKey" ]]; then
+        echo "Cant mount..."
+        echo "<?php phpinfo(); ?>" > /var/www/html/autoscale/index.php
+else
+# mount shared disk
+mount -t cifs //$SharedStorageAccountName.file.core.windows.net/$SharedAzureFileName /usr/share/nginx/html -o uid=$(id -u www-data),vers=2.1,username=$SharedStorageAccountName,password=$SharedStorageAccountKey,dir_mode=0770,file_mode=0770
+fi
 
 wget https://raw.githubusercontent.com/juliosene/azure-ubuntu-nginx-php-autoscale/master/default
 
